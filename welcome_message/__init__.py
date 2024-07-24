79d48a82435a705a76d73c4e76459d7f09a59758
DreamingLri <3507415455@qq.com>
2024-07-24 14:14:13 +0800

    add en_us

--- welcome_message/__init__.py
+++ welcome_message/__init__.py
@@ -14,8 +14,6 @@ default_config = {
         "欢迎 {player} 加入服务器喵~",
         "欢迎 {player} 喵~"
         ],
-    "datetime": "1988-01-01",
-    "error_message": "出错了喵，请联系服主喵~"
 }
 
 # help_info = '''-------- &a Welcome Message &r--------
@@ -26,25 +24,27 @@ default_config = {
 # ------------------------------------
 # '''   
 
-def get_welcome_message_list(server, context):
+def list_welcome_message(server, context):
     if 'index' in context:
         index = context['index'] - 1
     else:
         index = 0
     if index < 0:
-        return server.reply("查询页数不能小于1")
+        return server.reply(replace_code(_tr('message.page_error')))
     
     global page_size
     message_list = load_config()
     result_list = []
+    index = 1
     for message in message_list['messages']:
-        result_list.append(f'&r|- &a{message}&r')
+        result_list.append(f'&r|- &f{index}: &a{message}&r')
+        index += 1
     
     total = len(result_list)
-    resp = '------ &a消息列表 &r------\n'
+    resp = replace_code(_tr('message.header'))
 
     if total == 0:
-        resp += f'&r>>>>>> 第0页/共0页 <<<<<<'
+        resp += replace_code(_tr('message.footer_zero'))
     else:
         pages = math.ceil(total / page_size)
         if 0 <= index < pages:
@@ -52,50 +52,49 @@ def get_welcome_message_list(server, context):
         elif index == pages - 1:
             cur_page = result_list[index * page_size:]
         else:
-            return server.reply("查询页数超出范围")
+            return server.reply(replace_code(_tr('message.page_error')))
         for message in cur_page:
             resp += message + '\n'
-        resp += f'&r>>>>>> 第{index + 1}页/共{pages}页 <<<<<<'
+        resp += replace_code(_tr('message.footer'))
     return server.reply(replace_code(resp))
 
 def add_welcome_message(server, context):
     if __mcdr_.get_permission_level(context['player']) < 3:
-        return server.reply(replace_code(f'&c你没有权限添加消息喵~'))
+        return server.reply(replace_code(_tr('command.no_permission_add')))
     message = context['text']
     message_list = load_config()
     message_list['messages'].append(message)
     with open(config_path, 'w') as file:
         json.dump(message_list, file, indent=4)
-    return server.reply(replace_code(f'添加消息&a{message}&r成功喵~'))
+    return server.reply(replace_code(_tr('command.add_success')))
 
 def del_welcome_message(server, context):
     if __mcdr_.get_permission_level(context['player']) < 3:
-        return server.reply(replace_code(f'&c你没有权限删除消息喵~'))
-    message = context['text']
+        return server.reply(replace_code(_tr('command.no_permission_del')))
+    index = context['index']
     message_list = load_config()
-    if message in message_list['messages']:
-        message_list['messages'].remove(message)
-        with open(config_path, 'w') as file:
-            json.dump(message_list, file, indent=4)
-        return server.reply(replace_code(f'删除消息&a{message}&r成功喵~'))
-    else:
-        return server.reply(replace_code(f'消息&a{message}&r不存在喵~'))
+    if index < 1 or index > len(message_list['messages']):
+        return server.reply(replace_code(_tr('message.message_not_found')))
+    message = message_list['messages'].pop(index - 1)
+    with open(config_path, 'w') as file:
+        json.dump(message_list, file, indent=4)
+    return server.reply(replace_code(_tr('command.del_success')))
     
 
 def show_help_info(context: PlayerCommandSource):
     server = context.get_server()
     info = context.get_info()
     server.reply(info, "-------- §a Welcome Message §r--------")
-    server.reply(info, RText("§7!!wm help§r").set_hover_text(_tr("command.hover_hint")).set_click_event(RAction.suggest_command, "!!wm help") + ' ' + _tr("help.help"))
-    server.reply(info, RText("§7!!wm add <text>§r").set_hover_text(_tr("command.hover_hint")).set_click_event(RAction.suggest_command, "!!wm add ") + ' ' + _tr("help.add"))
-    server.reply(info, RText("§7!!wm delete <text>§r").set_hover_text(_tr("command.hover_hint")).set_click_event(RAction.suggest_command, "!!wm delete") + ' ' + _tr("help.delete"))
-    server.reply(info, RText("§7!!wm list§r").set_hover_text(_tr("command.hover_hint")).set_click_event(RAction.suggest_command, "!!wm list") + ' ' + _tr("help.list"))
+    server.reply(info, RText("§7!!wm help§r").set_hover_text(_tr("command.hover_hint") + " §7!!wm help§r").set_click_event(RAction.suggest_command, "!!wm help") + ' ' + _tr("help.help"))
+    server.reply(info, RText("§7!!wm add <text>§r").set_hover_text(_tr("command.hover_hint") + " §7!!wm add§r").set_click_event(RAction.suggest_command, "!!wm add ") + ' ' + _tr("help.add"))
+    server.reply(info, RText("§7!!wm del <index>§r").set_hover_text(_tr("command.hover_hint") + " §7!!wm del§r").set_click_event(RAction.suggest_command, "!!wm delete") + ' ' + _tr("help.delete"))
+    server.reply(info, RText("§7!!wm list [index]§r").set_hover_text(_tr("command.hover_hint") + " §7!!wm list§r").set_click_event(RAction.suggest_command, "!!wm list") + ' ' + _tr("help.list"))
     server.reply(info, "------------------------------------")
 
 def send_message(server: ServerInterface, player: str):
     message_list = load_config()
     if len(message_list['messages']) == 0:
-        server.execute('tellraw %s [{"text":"%s"}]' % (player, replace_code(message_list['error_message'])))
+        server.tell(player, _tr('message.empty'))
     else:
         message = random.choice(message_list['messages'])
         message = message.replace('{player}', player)
@@ -117,7 +116,7 @@ def replace_code(msg: str) -> str:
     return msg.replace('&', '§')
 
 def _tr(tag: str, *args):
-    _str = PluginServerInterface.get_instance().tr(f'mcdr_announcements.{tag}', *args)
+    _str = PluginServerInterface.get_instance().tr(f'welcome_message.{tag}', *args)
     return _str
 
 #=======================================================================================================================
@@ -130,13 +129,13 @@ def on_load(server: PluginServerInterface, old):
     global __mcdr_
     __mcdr_ = server
 
-    server.register_help_message('!!wm', '欢迎消息插件')
+    server.register_help_message('!!wm', _tr('register'))
 
     command_builder = SimpleCommandBuilder()
-    command_builder.command('!!wm list', get_welcome_message_list)
-    command_builder.command('!!wm list <index>', get_welcome_message_list)
+    command_builder.command('!!wm list', list_welcome_message)
+    command_builder.command('!!wm list <index>', list_welcome_message)
     command_builder.command('!!wm add <text>', add_welcome_message)
-    command_builder.command('!!wm del <text>', del_welcome_message)
+    command_builder.command('!!wm del <index>', del_welcome_message)
     command_builder.command('!!wm', show_help_info)
     command_builder.command('!!wm help', show_help_info)
     command_builder.arg('text', Text)
